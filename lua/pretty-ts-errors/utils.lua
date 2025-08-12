3c896bbd9d0cecab151e33ddd10b8b43993b5ef0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-08-13 01:30:24 +0700

    Add deno support

--- lua/pretty-ts-errors/utils.lua
+++ lua/pretty-ts-errors/utils.lua
@@ -27,7 +27,7 @@ function M.update_buffer(buf, contents)
 end
 
 function M.is_ts_source(source)
-	return vim.tbl_contains({ "tsserver", "ts", "typescript" }, source)
+	return vim.tbl_contains({ "tsserver", "ts", "typescript", "deno-ts" }, source)
 end
 
 function M.is_windows()
